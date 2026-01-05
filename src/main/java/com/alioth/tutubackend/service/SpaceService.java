9970f210600b1dd20f56ac4ba81e36c6742cf8ff
Alioth <20231797@stu.nmu.edu.cn>
2026-01-05 20:29:39 +0800

    feat(space): 添加空间服务接口及实现
    - 创建SpaceService接口定义空间操作方法
    - 实现空间查询条件构建功能
    - 实现空间信息包装转换功能
    - 实现分页空间列表包装功能
    - 实现空间信息校验功能
    - 实现根据空间级别自动填充限额功能
    - 集成用户服务进行关联查询
    - 添加空间级别枚举验证机制

---
+++ src/main/java/com/alioth/tutubackend/service/SpaceService.java
@@ -0,0 +1,54 @@
+package com.alioth.tutubackend.service;
+
+import com.alioth.tutubackend.model.dto.space.SpaceQueryRequest;
+import com.alioth.tutubackend.model.entity.Space;
+import com.alioth.tutubackend.model.vo.SpaceVO;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @author Alioth
+ * @description 针对表【space(空间)】的数据库操作Service
+ * @createDate 2026-01-05 19:35:28
+ */
+public interface SpaceService extends IService<Space> {
+    /**
+     * 获取查询条件
+     *
+     * @param spaceQueryRequest 空间查询请求
+     * @return 查询条件
+     */
+    QueryWrapper<Space> getQueryWrapper(SpaceQueryRequest spaceQueryRequest);
+
+    /**
+     * 获取空间信息包装类
+     *
+     * @param space 空间信息
+     * @return 空间信息包装类
+     */
+    public SpaceVO getSpaceVO(Space space);
+
+    /**
+     * 分页获取空间列表包装类
+     *
+     * @param spacePage 空间列表
+     * @return 空间列表包装类
+     */
+    Page<SpaceVO> getSpaceVOPage(Page<Space> spacePage);
+
+    /**
+     * 校验空间信息
+     *
+     * @param space 空间信息
+     * @param add   是否为创建校验
+     */
+    void validSpace(Space space, boolean add);
+
+    /**
+     * 根据空间级别填充空间信息
+     *
+     * @param space 空间信息
+     */
+    void fillSpaceBySpaceLevel(Space space);
+}
